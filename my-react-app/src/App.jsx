56488d08eab8df3d411e891e1b27775885799268
Gie2lah <yogimulyana498@gmail.com>
2023-06-23 18:58:03 +0700

    Menambahkan beberapa package

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,7 +1,11 @@
-import "./App.css";
-
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="bg-slate-800 min-h-screen flex justify-center items-center">
+        <h1 className="text-3xl font-bold text-white">Genpi Dumai</h1>
+      </div>
+    </>
+  );
 }
 
 export default App;
