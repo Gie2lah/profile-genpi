4c5a113b32e322b48bcc034ec65ffabbcedf6f78
Gie2lah <yogimulyana498@gmail.com>
2023-06-24 21:16:28 +0700

    merubah index.css

--- my-react-app/src/assets/pages/homePage.tsx
+++ my-react-app/src/assets/pages/homePage.tsx
@@ -4,8 +4,8 @@ const HomePage = () => {
   return (
     <>
       <div className="min-h-screen flex flex-col justify-center items-center bg-slate-800 space-y-4">
-        <h2 className="font-semiblod text-xl text-white">Welcome to</h2>
-        <h1 className="text-5xl font-bold text-white">GenPI Dumai</h1>
+        <h4 className="text-white">Welcome to</h4>
+        <h1 className="text-white">GenPI Dumai</h1>
         <p className="text-white">
           <q>Mohlah Ke Dumai</q>
         </p>
